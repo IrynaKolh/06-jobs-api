e3d8e354422074f264afea5b3765c893dfc3ec48
Iryna <iryna.kolhanova@gmail.com>
2023-12-05 15:58:29 -0500

    feat: hash password

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,19 @@
+const User = require("../models/User");
+const { StatusCodes } = require("http-status-codes");
+const { BadRequestError } = require("../errors");
+const bcrypt = require("bcryptjs");
+
 const register = async (req, res) => {
-  res.send("User has been registrated");
+  const { name, email, password } = req.body;
+  // if (!name || !email || !password) {
+  //   throw new BadRequestError("Please provide name, email and password");
+  // }
+  const salt = await bcrypt.genSalt(10);
+  const hassedPassword = await bcrypt.hash(password, salt);
+  const tempUser = { name, email, password: hassedPassword };
+
+  const user = await User.create({ ...tempUser });
+  res.status(StatusCodes.CREATED).json({ user });
 };
 
 const login = async (req, res) => {
---
+++ models/User.js
@@ -0,0 +1,34 @@
+const moggoose = require("mongoose");
+
+const UserSchema = new moggoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please provide a name"],
+    minlength: [3, "Name must be at least 3 characters"],
+    maxlength: [50, "Name must be no more than 50 characters"],
+  },
+  email: {
+    type: String,
+    required: [true, "Please provide a email"],
+    match: [
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      "Please provide a valid email",
+    ],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Please provide a password"],
+    minlength: [6, "Password must be at least 6 characters"],
+    // match: [
+    //   /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+])[A-Za-z\d!@#$%^&*()_+]{6,12}$/,
+    //   "Please provide a valid password (6-12 symbols that include at least 1 capital letter, at least 1 digit and at least 1 special symbol)",
+    // ],
+  },
+  isSeller: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+module.exports = moggoose.model("User", UserSchema);
